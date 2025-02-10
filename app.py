3101036d00dcac713fd58c3823b107ccffff459f
Isa Abdul-Hamid <isaabdulhamid13@gmail.com>
2025-02-10 16:21:45 -0400

    Removed pam from wsgi.py

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ from models import Admin, Category, RegularUser, Todo, TodoCategory, db, User
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'MySecretKey'
-app.config['JWT_ACCESS_COOKIE_NAME'] = 'access_token'
-app.config['JWT_REFRESH_COOKIE_NAME'] = 'refresh_token'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "MySecretKey"
+app.config["JWT_ACCESS_COOKIE_NAME"] = "access_token"
+app.config["JWT_REFRESH_COOKIE_NAME"] = "refresh_token"
 app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
 app.config["JWT_COOKIE_SECURE"] = True
 app.config["JWT_SECRET_KEY"] = "super-secret"
@@ -30,32 +30,38 @@ CORS(app)
 
 jwt = JWTManager(app)
 
+
 # customn decorator authorize routes for admin or regular user
 def login_required(required_class):
-  def wrapper(f):
-      @wraps(f)
-      @jwt_required()  # Ensure JWT authentication
-      def decorated_function(*args, **kwargs):
-        user = required_class.query.filter_by(username=get_jwt_identity()).first()  
-        print(user.__class__, required_class, user.__class__ == required_class)
-        if user.__class__ != required_class:  # Check class equality
-            return jsonify(message='Invalid user role'), 403
-        return f(*args, **kwargs)
-      return decorated_function
-  return wrapper
+    def wrapper(f):
+        @wraps(f)
+        @jwt_required()  # Ensure JWT authentication
+        def decorated_function(*args, **kwargs):
+            user = required_class.query.filter_by(username=get_jwt_identity()).first()
+            print(user.__class__, required_class, user.__class__ == required_class)
+            if user.__class__ != required_class:  # Check class equality
+                return jsonify(message="Invalid user role"), 403
+            return f(*args, **kwargs)
+
+        return decorated_function
+
+    return wrapper
+
 
 def login_user(username, password):
-  user = User.query.filter_by(username=username).first()
-  if user and user.check_password(password):
-    token = create_access_token(identity=username)
-    response = jsonify(access_token=token)
-    set_access_cookies(response, token)
-    return response
-  return jsonify(message="Invalid username or password"), 401
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        token = create_access_token(identity=username)
+        response = jsonify(access_token=token)
+        set_access_cookies(response, token)
+        return response
+    return jsonify(message="Invalid username or password"), 401
 
-@app.route('/')
+
+@app.route("/")
 def index():
-  return '<h1>mY Todo API</h1>'
+    return "<h1>mY Todo API</h1>"
+
 
 # Task 3.1 Here
 
@@ -80,13 +86,13 @@ def index():
 
 # Task 5.5 Here DELETE /todos/id
 
-@app.route('/todos/stats', methods=['GET'])
+
+@app.route("/todos/stats", methods=["GET"])
 @login_required(RegularUser)
 def get_stats_view():
-  user = RegularUser.query.filter_by(username=get_jwt_identity()).first()
-  return jsonify(num_todos=user.getNumTodos(),
-                 num_done=user.getDoneTodos()), 200
+    user = RegularUser.query.filter_by(username=get_jwt_identity()).first()
+    return jsonify(num_todos=user.getNumTodos(), num_done=user.getDoneTodos()), 200
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host="0.0.0.0", debug=True)
